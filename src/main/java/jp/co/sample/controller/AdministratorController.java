b7ea95c09948744b75ef944e0921c80d2c4e7d07
TaichiYazaki <taichi.yazaki@rakus-partners.co.jp>
2022-02-07 14:00:14 +0900

    controllerにログインメソッドを記述

--- src/main/java/jp/co/sample/controller/AdministratorController.java
+++ src/main/java/jp/co/sample/controller/AdministratorController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import jp.co.sample.domain.Administrator;
 import jp.co.sample.form.InsertAdministratorForm;
+import jp.co.sample.form.LoginForm;
 import jp.co.sample.service.AdministratorService;
 
 @Controller
@@ -43,4 +44,17 @@ public class AdministratorController {
 		model.addAttribute("ad", ad);
 		return "/";	
 	}
+	
+	//ログインフォームのメソッド
+	@ModelAttribute
+	public LoginForm setUpLoginForm() {
+		LoginForm loginForm = new LoginForm();
+		return loginForm;
+	}
+	@RequestMapping("/")
+	public String toLogin() {
+		return "administrator/login.html";
+	}
+	
+	
 }
