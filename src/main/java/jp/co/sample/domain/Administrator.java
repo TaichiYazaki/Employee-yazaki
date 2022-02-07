2d9a95aec805d7bfa8813496bd355ab7bc0cfdd7
TaichiYazaki <taichi.yazaki@rakus-partners.co.jp>
2022-02-07 09:37:25 +0900

    administratorクラスの作成

---
+++ src/main/java/jp/co/sample/domain/Administrator.java
@@ -0,0 +1,61 @@
+package jp.co.sample.domain;
+
+public class Administrator {
+
+	private Integer id;
+	private String name;
+	private String mailAdress;
+	private String password;
+	
+	public Administrator() {}
+	
+
+	public Administrator(Integer id, String name, String mailAdress, String password) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.mailAdress = mailAdress;
+		this.password = password;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMailAdress() {
+		return mailAdress;
+	}
+
+	public void setMailAdress(String mailAdress) {
+		this.mailAdress = mailAdress;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "Administrator [id=" + id + ", name=" + name + ", mailAdress=" + mailAdress + ", password=" + password
+				+ "]";
+	}
+	
+	
+}
